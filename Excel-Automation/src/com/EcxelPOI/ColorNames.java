a7e71927b2d6f37b4b89383ae1c8c74158ca54d6
Pawarraj <rpawar588@gmail.com>
2022-11-07 11:34:11 +0530

    Excel-Automation

---
+++ Excel-Automation/src/com/EcxelPOI/ColorNames.java
@@ -0,0 +1,55 @@
+package com.EcxelPOI;
+
+import java.io.FileOutputStream;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ColorNames 
+{
+
+	public static void main(String[] args) 
+	{
+		Writecontent();
+	}
+	private static void Writecontent()
+	{
+		FileOutputStream fout=null;
+		Workbook wb=null;
+		Sheet sh=null;
+		Row row=null;
+		Cell cell=null;
+		try
+		{
+			wb= new XSSFWorkbook();
+			sh=wb.createSheet("Sheet1");
+			row=sh.createRow(9);
+			for(int i=0;i<20;i++)
+			{
+				cell=row.createCell(i);	
+				cell.setCellValue("color"+i);
+			}
+			fout=new FileOutputStream("D:\\EXCEL\\Assignment2.xlsx");
+			wb.write(fout);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				fout.close();
+				wb.close();
+			}catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
+
